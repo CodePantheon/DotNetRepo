8d48e89e1a79f47a474c2813c31a97240c38c2c9
CodePantheon <codepantheon@gmail.com>
2019-06-22 20:16:41 +0530

    Added Tests for Subraction

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -32,5 +32,84 @@ namespace CalculatorLibTests
             // Assert
             Assert.AreEqual(9, sum);
         }
+
+        [TestMethod]
+        public void Test_Add_NegativeNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double sum = calculator.Add(-5.6, 10.6);
+
+            // Assert
+            Assert.AreEqual(5, sum);
+        }
+
+        [TestMethod]
+        public void Test_Subtract_WholeNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double difference = calculator.Subtract(5, 3);
+
+            // Assert
+            Assert.AreEqual(2, difference);
+        }
+
+        [TestMethod]
+        public void Test_Subtract_FractionalNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double difference = calculator.Subtract(5.3, 2.3);
+
+            // Assert
+            Assert.AreEqual(3, difference);
+        }
+
+        [TestMethod]
+        public void Test_Subtract_MinuedNegative()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double difference = calculator.Subtract(-5.3, 2.3);
+
+            // Assert
+            Assert.AreEqual(-7.6, difference);
+        }
+
+        [TestMethod]
+        public void Test_Subtract_BothNegativeNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double difference = calculator.Subtract(-5.3, -2.3);
+
+            // Assert
+            Assert.AreEqual(-3.0, difference);
+        }
+
+        [TestMethod]
+        public void Test_Subtract_MinuedLessThanSubtrahend()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double difference = calculator.Subtract(5.3, 7.3);
+
+            // Assert
+            Assert.AreEqual(-2, difference);
+        }
+
     }
 }
