825d8c95e83d8ba93678a06b6b56815b01582a89
CodePantheon <codepantheon@gmail.com>
2019-06-22 19:12:50 +0530

    Calculator Implementation with Unit testing

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CalculatorLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorLibTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Test_Add_WholeNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double sum = calculator.Add(3, 5);
+
+            // Assert
+            Assert.AreEqual(8, sum);
+        }
+
+        [TestMethod]
+        public void Test_Add_FractionalNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            double sum = calculator.Add(3.7, 5.3);
+
+            // Assert
+            Assert.AreEqual(9, sum);
+        }
+    }
+}
