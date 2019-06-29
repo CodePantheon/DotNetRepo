521efa6abaf2c999905dc7900f1b50fd8a5a4a12
Trivedi-Ashish <ashishtrivedi28@gmail.com>
2019-06-29 23:49:35 +0530

    Logger class impl

--- Logger/LoggerLib/ILogger.cs
+++ Logger/LoggerLib/ILogger.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LoggerLib
 {
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Interface for Logger types.
+    /// </summary>
     public interface ILogger
     {
-        void LogInfo(string infoText);
+        void LogInfo(string infoText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "");
 
-        void LogWarning(string warningText);
+        void LogWarning(string warningText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "");
 
-        void LogError(string errorText);
+        void LogError(string errorText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "");
     }
 }
--- Logger/LoggerLib/Logger.cs
+++ Logger/LoggerLib/Logger.cs
@@ -1,33 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LoggerLib
 {
-    internal class Logger : ILogger
+    using System;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Logger class.
+    /// </summary>
+    public class Logger : ILogger
     {
         private IWriter writer;
+        private readonly string logTime = DateTime.Now.ToString();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class.
+        /// </summary>
+        /// <param name="writer"></param>
         public Logger(IWriter writer)
         {
             this.writer = writer;
         }
 
-        public void LogError(string errorText)
+        public void LogInfo(string infoText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "")
         {
-            throw new NotImplementedException();
+            this.writer.Write(string.Format("{0} {1} {2} {3}", logTime, infoText, callerFilePath, memberName));
         }
 
-        public void LogInfo(string infoText)
+        public void LogWarning(string warningText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "")
         {
-            throw new NotImplementedException();
+            this.writer.Write(string.Format("{0} {1} {2} {3}", logTime, warningText, callerFilePath, memberName));
         }
 
-        public void LogWarning(string warningText)
+        public void LogError(string errorText, [CallerMemberName] string memberName = "", [CallerFilePath] string callerFilePath = "")
         {
-            throw new NotImplementedException();
+            this.writer.Write(string.Format("{0} {1} {2} {3}", logTime, errorText, callerFilePath, memberName));
         }
     }
 }
